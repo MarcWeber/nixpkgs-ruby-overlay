b0535c86fbd3915755c5e025f9211e411fdbe768
Marc Weber <marco-oweber@gmx.de>
2010-11-24 18:41:54 +0100

    initial - some packages can be compiled

---
+++ default.nix
@@ -0,0 +1,157 @@
+# same args as in all-packages.nix
+# should this be moved into pkg/top-level/all-packages.nix ?
+{
+  system ? builtins.currentSystem
+, stdenvType ? system
+, bootStdenv ? null
+, noSysDirs ? true
+, gccWithCC ? true
+, gccWithProfiling ? true
+, config ? null
+
+# additional:
+, nixpkgs ? ../nixpkgs
+}:
+
+/* overview:
+
+   gem nix fetches all known packgaes from default sources (rubygems, gemcutter).
+   Those packages are written to nix package list pkgs/defaults.nix.
+   resolveRubyPkgDependencies then tries to find dependiencies automatically
+   failing if dependency constraints can't be satisfied. A very simple
+   implementation is used.
+
+   C libraries (and missing deps) are merged into those attributes.
+   (See patches in pkgs/defaults.nix)
+
+   finally the tested attribute contains ruby packgaes which are known to work.
+
+   Of course it should be easy to merge in your own ruby packages..
+*/
+
+let
+
+  # stupidly repeating all arguments:
+  mainConfig = { inherit system stdenvType bootStdenv noSysDirs gccWithCC gccWithProfiling config; };
+
+  merge = list: (removeAttrs (mergeAttrsByFuncDefaults list) ["mergeAttrBy"]);
+
+  # pkgs, lib, getConfig from nixpkgs:
+  pkgs = import nixpkgs mainConfig;
+  lib = pkgs.lib;
+  getConfig = pkgs.getConfig;
+
+  inherit (builtins) attrNames head tail compareVersions lessThan filter hasAttr getAttr;
+
+  inherit (lib) attrSingleton mergeAttrsByFuncDefaults optional listToAttrs;
+
+  ruby_defaults = pkgs.callPackage pkgs/defaults.nix { inherit pkgs; };
+
+  # roselves dependencies automatically ensuring that only one version of a
+  # library is present in the dependency chain by ignoring all "older" versions
+  # of a package. So this KISS solver is likely miss some valid solutions which
+  # reqiure older package versions
+  #
+  # returns: list of derivation. attr names are package names (without version)
+  resolveRubyPkgDependencies = { platform ? "ruby",  # platform. ruby tested only.
+                                  rubyPackages ? {},     # dict like pkgs/ruby-packages.nix
+                                                         # mandatory keys: name, version, ...
+                                  names ? [],   # the packages you'd like to use (list of names)
+                                  patches ? {}, # some dependencies require C extensions
+                                  rubyDerivation ? (args: throw "no function specified") # creates the derivation
+      }:
+
+    let latestPkg = list: builtins.head (lib.sort (a: b: builtins.compareVersions a.version b.version) list);
+
+        # result is attrs containing package names only
+        latestByName = listToAttrs ( lib.mapAttrsFlatten
+                            (pkg_name : attr:
+                              let latest = latestPkg (lib.attrValues attr);
+                              in  { name = latest.name;
+                                    value = latest;
+                                  }
+                            ) (getAttr platform rubyPackages) );
+
+        pkgByName = depending: name: lib.maybeAttr name (throw "couldn't find ruby dependency named ${name} required by ${depending}") latestByName;
+
+        pkgByConstraints = depending: p:
+          let name = head p;
+              constraints = head (tail p);
+              pkg = pkgByName depending name;
+              match = op_version:
+                let op = head op_version;
+                    v = head (tail op_version);
+                    x = compareVersions pkg.version v;
+                    fs = {
+                      "="  = x == 0;
+                      "!=" = x != 0;
+                      ">"  = lessThan 0 x;
+                      "<"  = lessThan x 0;
+                      ">=" = x == 0 || lessThan 0 x;
+                      "<=" = x == 0 || lessThan x 0;
+                      "~>" = lessThan 0 x && lessThan (compareVersions pkg.bump v) 0;
+                    };
+                in getAttr op fs;
+          failing = lib.filter (x: !(match x)) constraints;
+
+          in if failing ==[] then pkg else (throw "couldn't satisfy all contstraints of depndency ${name} reqiured by ${depending}: ${builtins.toXML failing}");
+
+        makeDerivation =
+              making: # list of names being visited to prevent cyclic dependencies
+              pkg_descr:
+          let full_name = "${pkg_descr.name}-${pkg_descr.version}";
+              patchesList = optional (hasAttr full_name patches) (getAttr full_name patches)
+                     ++ optional (hasAttr pkg_descr.name patches) (getAttr pkg_descr.name patches);
+              patched_descr = merge ([pkg_descr] ++ patchesList);
+              nameToConstraint = f: [f []];
+              ruby_deps = patched_descr.runtimeDependencies 
+                          ++ (map nameToConstraint (lib.maybeAttr "additionalRubyDependencies" [] patched_descr));
+              making_new = making ++ [pkg_descr.name];
+              deps_derivations = map (x: makeDerivation making_new (pkgByConstraints pkg_descr.name x)) ruby_deps;
+
+          in if  lib.elem pkg_descr.name making then throw "cyclic dependency ${builtins.toXML making} -> ${pkg_descr.name}"
+              else rubyDerivation (merge [ patched_descr { propagatedBuildInputs = deps_derivations; } ]);
+
+    in listToAttrs (map (name: { inherit name; value = makeDerivation [] (pkgByName "<user>" name); } ) names)
+  ; # end resolveRubyPkgDependencies
+   
+  a = rec {
+
+    inherit ruby_defaults;
+
+    rubyPackages = names: resolveRubyPkgDependencies {
+      inherit (ruby_defaults) rubyPackages patches rubyDerivation;
+      inherit names;
+    };
+
+    # packages known to work:
+    tested = rubyPackages ["nokogiri" "rake" "escape"
+          "git"
+          "hoe"
+          "rubyforge"
+          "json_pure"
+          "chronic"
+          "sup"
+          "rubygems_update"
+          "jeweler"
+          "rake"
+          "ncursesw"
+          "trollop"
+          "gettext"
+          "locale"
+          "lockfile"
+          "rmail"
+          "highline"
+          "net_ssh"
+          "xrefresh_server"
+          "mime_types"
+      ];
+
+    # xapian
+    # xapianBindings
+
+    inherit resolveRubyPkgDependencies;
+
+  };
+
+in a
---
+++ pkgs/defaults.nix
@@ -0,0 +1,134 @@
+{writeScript, pkgs}:
+
+let inherit (pkgs) fetchurl stdenv;
+    inherit (pkgs.lib) mergeAttrsByFuncDefaults optional;
+    inherit (pkgs) rubygems ruby;
+    inherit (builtins) hasAttr getAttr;
+
+in rec {
+
+  rubyPackages = pkgs.callPackage ./ruby-packages.nix {};
+
+  patchUsrBinEnv = writeScript "path-usr-bin-env" ''
+    #!/bin/sh
+    set -x
+    echo "==================="
+    find "$1" -type f -name "*.rb" | xargs sed -i "s@/usr/bin/env@$(type -p env)@g"
+    find "$1" -type f -name "*.mk" | xargs sed -i "s@/usr/bin/env@$(type -p env)@g"
+  '';
+
+  # these settings are merged into the automatically generated settings
+  # either the nameNoVersion or name must match
+  patches = {
+    sup = {
+      additionalRubyDependencies = ["ncursesw"];
+      buildInputs = [ pkgs.ncurses pkgs.xapianBindings ];
+    };
+    sqlite3_ruby = { propagatedBuildInputs = [ pkgs.sqlite ]; };
+    # rails = {
+    #   gemFlags = "--no-ri --no-rdoc";
+    #   additionalRubyDependencies = [ "mime_types" ];
+    #   propagatedBuildInputs = [ libs.mime_types libs.rake ];
+    # };
+    ncurses = { buildInputs = [ pkgs.ncurses ]; };
+    ncursesw = { buildInputs = [ pkgs.ncurses ]; };
+    nokogiri = {
+      buildFlags=["--with-xml2-dir=${pkgs.libxml2} --with-xml2-include=${pkgs.libxml2}/include/libxml2"
+                  "--with-xslt-dir=${pkgs.libxslt}" ];
+    };
+
+    ffi = {
+      postUnpack = "onetuh";
+      buildInputs = [ pkgs.rake ];
+      buildFlags=["--with-ffi-dir=${pkgs.libffi}"];
+      NIX_POST_EXTRACT_FILES_HOOK = patchUsrBinEnv;
+    };
+
+    xrefresh_server =
+    let patch = fetchurl {
+        url = "http://mawercer.de/~nix/xrefresh.diff.gz";
+        sha256 = "1f7bnmn1pgkmkml0ms15m5lx880hq2sxy7vsddb3sbzm7n1yyicq";
+      };
+    in {
+      additionalRubyDependencies = [ "rb_inotify" ];
+
+      # monitor implementation for Linux
+      postInstall = ''
+        cd $out/gems/*;
+        cat ${patch} | gunzip | patch -p 1;
+      '';
+    };
+
+    xapian_full = {
+      buildInputs = [ pkgs.rake pkgs.zlib pkgs.libuuid ];
+    };
+  };
+
+
+  rubyDerivation = args :
+    let full_name = "${args.name}-${args.version}";
+        completeArgs = (mergeAttrsByFuncDefaults
+        ([
+          {
+            buildInputs = [rubygems ruby pkgs.makeWrapper];
+            unpackPhase = ":";
+            configurePhase=":";
+            bulidPhase=":";
+
+            # TODO add some abstraction for this kind of env path concatenation. It's used multiple times
+            installPhase = ''
+              ensureDir "$out/nix-support"
+              export HOME=$TMP/home; mkdir "$HOME"
+
+              gem install -V --ignore-dependencies -i "$out" "$src" $gemFlags -- $buildFlags
+              rm -fr $out/cache # don't keep the .gem file here
+
+              THIS_RUBY_LIB=$(echo $out/gems/*/lib)
+              THIS_GEM_PATH=$out
+
+              cat >> $out/nix-support/setup-hook << EOF 
+                declare -A RUBYLIB_HASH # using bash4 hashs
+                declare -A GEM_PATH_HASH # using bash4 hashs
+
+                if [ -n "$THIS_RUBY_LIB" ]; then
+                  RUBYLIB_HASH["$THIS_RUBY_LIB"]=
+                fi
+                for path in \''${!RUBYLIB_HASH[@]}; do
+                  export RUBYLIB=\''${RUBYLIB}\''${RUBYLIB:+:}\$path
+                done
+                GEM_PATH_HASH["$THIS_GEM_PATH"]=
+                for path in \''${!GEM_PATH_HASH[@]}; do
+                  export GEM_PATH=\''${GEM_PATH}\''${GEM_PATH:+:}\$path
+                done
+              EOF
+              . $out/nix-support/setup-hook
+
+              for prog in $out/bin/*; do
+                wrapProgram "$prog" \
+                  --prefix RUBYLIB : "$RUBYLIB":${rubygems}/lib \
+                  --prefix GEM_PATH : "$GEM_PATH" \
+                  --set RUBYOPT 'rubygems'
+              done
+
+              for prog in $out/gems/*/bin/*; do
+                [ -e "$out/bin/$(basename $prog)" ] && continue || true
+                sed -i '1s@.*@#!  ${ruby}/bin/ruby@' "$prog"
+                t="$out/bin/$(basename "$prog")"
+                cat >> "$t" << EOF
+              #!/bin/sh
+              export GEM_PATH=$GEM_PATH:\$GEM_PATH
+              #export RUBYLIB=$RUBYLIB:\$RUBYLIB
+              exec $(type -p ruby) $prog "\$@"
+              EOF
+                chmod +x "$t"
+              done
+
+              runHook postInstall
+            '';
+        }
+        args 
+      ]
+      ));
+    in stdenv.mkDerivation (removeAttrs completeArgs ["mergeAttrBy"]);
+
+}
